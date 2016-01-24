28aabca70e7d14b2d8a7a371cd15c6c21b4c7e44
Mattias Andersson <mandersson444@gmail.com>
2016-01-24 16:11:22 +0000

    Remove console.log

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,16 +8,13 @@ module.exports = function (app) {
 		.get(function (req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
-		
+
 	app.route('/:time')
 		.get(function(req, res) {
-			console.log("Reached the regexp route for", req.url);
 			let timeStr = req.params.time;
-			console.log(timeStr);
 			let timestamp;
 			let result = {unix: null, natural: null};
-			
-			console.log("Time string:", timeStr);
+
 			// If the time string contains non-numeric characters, attempt to parse it as as time string.
 			if (timeStr.match(/\D/)) {
 				timestamp = Date.parse(timeStr);
@@ -25,7 +22,7 @@ module.exports = function (app) {
 				// Question: Do we need to round to the start of the day?
 				timestamp = Number(timeStr * 1000);
 			}
-				
+
 			if (!isNaN(timestamp)) {
 				let theDate = new Date(timestamp);
 				result = {
